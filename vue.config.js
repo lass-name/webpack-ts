e6255cfe41cbaaff64bafe6e8ae011558fccc9c0
lass_name <zhangdianlian@163.com>
2019-11-21 20:41:57 +0800

    添加vue配置项，满足不同场景的发布打包

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
+  productionSourceMap: false,
   devServer: {
     proxy: {
       '/api': {
@@ -8,6 +11,25 @@ module.exports = {
           '^/api': ''
         }
       }
+    }
+  },
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        // parallel: true,
+        // sourceMap: false,
+        terserOptions: {
+          compress: {
+            drop_console: process.env.NODE_ENV === 'production' && !process.env.BUILD_MODE,
+            drop_debugger: process.env.NODE_ENV === 'production'
+          },
+          output: {
+            comments: false // remove comments
+          }
+        },
+        extractComments: false
+      })]
     }
   }
 }
